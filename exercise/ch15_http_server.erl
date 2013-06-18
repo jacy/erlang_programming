60a686356b3ea52335b1728e30a55c20b6d97d57
jacy <johnsoncr.hong@gmail.com>
2013-06-18 22:10:00 +0800

    refactor

--- exercise/ch15_http_server.erl
+++ exercise/ch15_http_server.erl
@@ -1,57 +1,45 @@
 -module(ch15_http_server).
 -compile(export_all).
 -define(PORT, 9999).
+-define(THIRTY_SECONDS, 30000).
 -define(OPTION,[{packet, 0}, {active, false}]).
 
-start() ->
-	Pid = spawn(?MODULE, init, []),
-	register(?MODULE, Pid).
+start() -> register(?MODULE, spawn_link(?MODULE, init, [])).
 
 %% stop() -> 
-%% 	exit(whereis(?MODULE)),
-%% 	unregister(?MODULE).
+%%  	exit(whereis(?MODULE)),
+%%  	unregister(?MODULE).
 
 init() ->
-	{ok,ListenSocket} = gen_tcp:listen(?PORT, ?OPTION),
-	listen(ListenSocket, [], 1).
+	{ok, ListenSocket} = gen_tcp:listen(?PORT, ?OPTION),
+	listen(ListenSocket, 1).
 
-%% handle_stop(ListenSocket) ->
-%% 	receive 
-%% 		stop -> close(ListenSocket);
-%% 		_ -> ok
-%% 	after 0 -> timeout
-%% 	end.
-	
-listen(ListenSocket, Sessions, Count) ->
-	io:format("Looping in count ~p ~n",[Count]),
+listen(ListenSocket, Count) ->
 	{ok, Connection} = gen_tcp:accept(ListenSocket),
-	
-	Pid = spawn(?MODULE, get_request, [Connection, Count]),
-	gen_tcp:controlling_process(Connection, Pid),
-	listen(ListenSocket,Sessions, Count + 1).
+	io:format("Accepted pid ~p, count ~p ~n",[self(), Count]),
+	_Pid = spawn(?MODULE, pull_request, [Connection, Count]),
+%%  gen_tcp:controlling_process(Connection, _Pid), % Why This is optional in this case ?????????????
+	listen(ListenSocket, Count + 1).
 
-
-get_request(Connection, Count) ->
-	case gen_tcp:recv(Connection, 0, 5000) of
+pull_request(Connection, Count) ->
+	case gen_tcp:recv(Connection, 0, ?THIRTY_SECONDS) of
 		{ok, Binary} -> 
-			handle(Binary, Connection),
-			get_request(Connection, Count);
-		{error, closed} -> io:format("Success close Request of count ~p ~n",[Count]);
+			io:format("Pull request form pid ~p, count ~p ,with body: ~n ~p ~n",[self(), Count, Binary]),
+			response(Binary, Connection),
+			close(Connection);
 		{error,timeout} -> 
-			io:format("Timeout of count ~p ~n",[Count]),
+			io:format("Pull Request timeout of count:~p ~n",[Count]),
 			close(Connection)
 	end.
 
-handle(Binary, Connection) ->
-	io:format("Request body ~p ~n",[Binary]),
+response(Binary, Connection) ->
 	LineBreak = "\r\n",
 	StatusLine = "HTTP/1.1 200 OK" ++ LineBreak,
 	RespBody = "Test",
-	RespHeader = lists:concat(["Content-Length: ",string:len(RespBody),LineBreak]),
-	Cookie = "Set-Cookie: JSESSIONID=83DF81AA079028B97C46BC93C2613AEF; expires=Thu, 18-Jun-2015 07:45:30 GMT; path=/;" ++ LineBreak,
+	RespHeader = lists:concat(["Content-Length: ",string:len(RespBody), LineBreak]),
+	Cookie = "Set-Cookie: JSESSIONID=TODO; expires=Thu, 18-Jun-2015 07:45:30 GMT; path=/;" ++ LineBreak,
 	
 	Resp = lists:concat([StatusLine,RespHeader,Cookie,LineBreak,RespBody,LineBreak]),
-	gen_tcp:send(Connection, Resp),
-	close(Connection).
+	gen_tcp:send(Connection, Resp).
 
 close(Socket) -> gen_tcp:close(Socket).
